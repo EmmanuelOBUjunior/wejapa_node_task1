73b86e7ae6e887adc631c8bdd422d560f736025c
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2020-07-24 07:21:47 +0100

    Done with task one

--- index.js
+++ index.js
@@ -1,31 +1,21 @@
 const { createServer } = require('http');
 
-const PORT = process.env.PORT || 5500;
+const PORT = process.env.PORT || 3005;
 
 
 const server = createServer((request, response) =>{
     if(request.method === 'GET' && request.url === '/'){
-        response.writeHead(200, {'Content-Type': 'text./html'});
+        response.writeHead(200, {'Content-Type': 'text/html'});
         response.write('<h2>Hello World, welcome to WeJapa Internships</h2>');
     }
     else{
-        response.writeHead(200, {'Content-Type': 'text./html'});
+        response.writeHead(200, {'Content-Type': 'text/html'});
         response.write('<h2>Error!!!...Invalid URL</h2>');
     }
 });
 
-// const server = createServer();
-// server.on("request", (request, response) =>{
-//     if(request.method === 'GET' && request.url === '/'){
-//         response.writeHead(200, {'Content-Type': 'text./html'});
-//         response.write('<h2>Hello World, welcome to WeJapa Internships</h2>');
-//     }
-//     else{
-//         response.writeHead(200, {'Content-Type': 'text./html'});
-//         response.write('<h2>Error!!!...Invalid URL</h2>');
-//     }
-// });
 
 server.listen(PORT, ()=>{
     console.log(`Starting server at port ${PORT}`);
 });
+
