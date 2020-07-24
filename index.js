51cb4be6c9abd6125c2d3aea9ce0da195aa459bb
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2020-07-24 06:22:46 +0100

    Working on task

---
+++ index.js
@@ -0,0 +1,32 @@
+const { create } = require('domain');
+const { createServer } = require('http');
+
+const PORT = process.env.PORT || 3000;
+
+
+const server = createServer((request, response) =>{
+    if(request.method === 'GET' && request.url === '/'){
+        response.writeHead(200, {'Content-Type': 'text./html'});
+        response.write('<h2>Hello World, welcome to WeJapa Internships</h2>');
+    }
+    else{
+        response.writeHead(200, {'Content-Type': 'text./html'});
+        response.write('<h2>Error!!!...Invalid URL</h2>');
+    }
+});
+
+// const server = createServer();
+// server.on("request", (request, response) =>{
+//     if(request.method === 'GET' && request.url === '/'){
+//         response.writeHead(200, {'Content-Type': 'text./html'});
+//         response.write('<h2>Hello World, welcome to WeJapa Internships</h2>');
+//     }
+//     else{
+//         response.writeHead(200, {'Content-Type': 'text./html'});
+//         response.write('<h2>Error!!!...Invalid URL</h2>');
+//     }
+// });
+
+server.listen(PORT, ()=>{
+    console.log(`Starting server at port ${PORT}`);
+});
