4ec05ee02de5dbed11a54e6fa5e4cc829a0d3aae
EmmanuelOBUjunior <obu.junior.emmanuel@gmail.com>
2020-07-24 07:33:24 +0100

    Done

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const PORT = process.env.PORT || 3005;
 
 
 const server = createServer((request, response) =>{
-    if(request.method === 'GET' && request.url === '/'){
+    if(request.method === 'GET' && request.url === "/"){
         response.writeHead(200, {'Content-Type': 'text/html'});
         response.write('<h2>Hello World, welcome to WeJapa Internships</h2>');
     }
